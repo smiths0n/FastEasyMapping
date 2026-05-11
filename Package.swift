dbf85acc8ee5efb956ac5380826dc2cafcc728dd
smiths0n <alex@smiths0n.co.uk>
2026-05-11 22:17:15 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "Updates",
+    platforms: [
+        .iOS("17.0")
+    ],
+    products: [
+        .library(
+            name: "FastEasyMapping",
+            targets: ["FastEasyMapping"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "FastEasyMapping",
+            dependencies: [],
+            path: "FastEasyMapping/"
+        )
+    ]
+)
